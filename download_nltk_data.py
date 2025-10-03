a7934c925c66d7700c52e5bb999b67b138fff1fe
Devanshu Markam <devanshuasm750@gmail.com>
2025-10-04 00:23:19 +0530

    download update

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -11,5 +11,6 @@ nltk.data.path.append(DOWNLOAD_DIR)
 # Download the necessary packages to that specific directory
 nltk.download("stopwords", download_dir=DOWNLOAD_DIR)
 nltk.download("punkt", download_dir=DOWNLOAD_DIR)
+nltk.download("punkt_tab", download_dir=DOWNLOAD_DIR)
 
 print("NLTK data downloaded successfully to the 'nltk_data' directory.")
